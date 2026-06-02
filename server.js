3a3c2a358224d92d1b82d5f82c2ce66f59e5b71b
Areeb Ali <areebktk8@gmail.com>
2026-06-02 12:17:55 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import app from './src/app.js';
+import connectDB from './src/config/db.js';
+
+connectDB();
+
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ src/models/user.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    lowercase: true,
+    trim: true,
+    match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "Please use a valid email address"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: [8, "Password must be at least 8 characters long"],
+  },
+}, {
+  timestamps: true,
+})
