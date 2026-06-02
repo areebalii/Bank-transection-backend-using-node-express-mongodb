554dd459d145ddc9d25648b3cb59fe8002518767
Areeb Ali <areebktk8@gmail.com>
2026-06-02 21:46:54 +0500

    feat: implement account creation functionality with error handling and update account model

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -1,2 +1,20 @@
 import AccountModel from "../models/account.model.js";
 
+export const createAccount = async (req, res) => {
+  try {
+    const user = req.user; // Get the authenticated user from the request
+
+    // Create a new account associated with the authenticated user
+    const account = await AccountModel.create({
+      user: user._id,
+    });
+
+    res.status(201).json({
+      message: "Account created successfully",
+      account
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
--- src/routes/account.route.js
+++ src/routes/account.route.js
@@ -1,6 +1,16 @@
 import express from 'express';
+import { authMiddleware } from '../middleware/auth.middleware.js';
+import { createAccount } from '../controllers/account.controller.js';
 
 const router = express.Router();
+
+/**
+ * @route   GET /api/accounts/
+ * @desc    create a new account
+ * @access  protected
+ */
+router.post("/", authMiddleware, createAccount)
+
 
 
 
